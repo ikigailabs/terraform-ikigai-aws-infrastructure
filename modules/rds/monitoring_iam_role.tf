d42bceac8c497bd970cba0f614d7cd459bc277a2
Prashanth Mohan <pmohan@ikigailabs.io>
2024-05-28 16:34:18 -0700

    fixed rds monitoring iam role

--- modules/rds/monitoring_iam_role.tf
+++ modules/rds/monitoring_iam_role.tf
@@ -1,8 +1,40 @@
-data "aws_iam_policy" "rds-monitoring-policy" {
-  name = "AmazonRDSEnhancedMonitoringRole"
+resource "aws_iam_role" "rds_monitoring_role" {
+  name = "RDSMonitorngRole"
+  assume_role_policy = file("${path.module}/monitoring_policy.json")
 }
 
-resource "aws_iam_role" "rds-monitoring-role" {
-  name = "rds_monitorng_role"
-  assume_role_policy = data.aws_iam_policy.rds-monitoring-policy.policy
+resource "aws_iam_role_policy" "rds_monitoring_role_policy" {
+  name = "RDSMonitoringRolePolicy"
+  role = aws_iam_policy.rds_monitoring_role.id
+  policy = jsonencode(
+    {
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Sid": "EnableCreationAndManagementOfRDSCloudwatchLogGroups",
+            "Effect": "Allow",
+            "Action": [
+                "logs:CreateLogGroup",
+                "logs:PutRetentionPolicy"
+            ],
+            "Resource": [
+                "arn:aws:logs:*:*:log-group:RDS*"
+            ]
+        },
+        {
+            "Sid": "EnableCreationAndManagementOfRDSCloudwatchLogStreams",
+            "Effect": "Allow",
+            "Action": [
+                "logs:CreateLogStream",
+                "logs:PutLogEvents",
+                "logs:DescribeLogStreams",
+                "logs:GetLogEvents"
+            ],
+            "Resource": [
+                "arn:aws:logs:*:*:log-group:RDS*:log-stream:*"
+            ]
+        }
+    ]
+}
+  )
 }
