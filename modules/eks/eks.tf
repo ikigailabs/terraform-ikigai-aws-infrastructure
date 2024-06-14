e7bba16d55a2360281089a3b1aef16e6f6568202
Prashanth Mohan <pmohan@ikigailabs.io>
2024-06-14 12:14:20 -0400

    removed disk_size default

--- modules/eks/eks.tf
+++ modules/eks/eks.tf
@@ -8,7 +8,7 @@ module "eks" {
 
   cluster_name    = var.cluster_name
   cluster_version = var.kubernetes_version
-  iam_role_name = aws_iam_role.node_instance_role.name
+  iam_role_name = aws_iam_role.node_instance_role.name # <- this isn't in platform_aws?
 
   vpc_id                         = var.vpc_id
   subnet_ids                     = [var.private_subnet_1_id, var.private_subnet_2_id]
@@ -16,7 +16,6 @@ module "eks" {
 
   eks_managed_node_group_defaults = {
     ami_type = var.ami_type
-    disk_size = 200
   }
 
   eks_managed_node_groups = {
